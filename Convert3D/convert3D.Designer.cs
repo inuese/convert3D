cf7d40094f14ae941aba47212834a1bd36ba3e63
esegit <39058682+inuese@users.noreply.github.com>
2019-01-26 15:58:51 +0900

    Program Upload

---
+++ Convert3D/convert3D.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Convert3D
+{
+    partial class convert3D
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(convert3D));
+            this.openBtn = new System.Windows.Forms.Button();
+            this.drawChart = new ChartDirector.WinChartViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(12, 12);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(81, 34);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // drawChart
+            // 
+            this.drawChart.Location = new System.Drawing.Point(12, 52);
+            this.drawChart.Name = "drawChart";
+            this.drawChart.Size = new System.Drawing.Size(300, 41);
+            this.drawChart.TabIndex = 1;
+            this.drawChart.TabStop = false;
+            this.drawChart.ViewPortChanged += new ChartDirector.WinViewPortEventHandler(this.drawChart_ViewPortChanged);
+            this.drawChart.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawChart_MouseDown);
+            this.drawChart.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawChart_MouseMove);
+            this.drawChart.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawChart_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "파일명 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 669);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawChart);
+            this.Controls.Add(this.openBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Convert3D";
+            ((System.ComponentModel.ISupportInitialize)(this.drawChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openBtn;
+        private ChartDirector.WinChartViewer drawChart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Convert3D/convert3D.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using ChartDirector;
+using OpenCvSharp;
+
+namespace Convert3D
+{
+    public partial class convert3D : Form
+    {
+        //차트를 움직이기 위한 파라미터의 멤버변수를 정의
+        double elevationAngle = 30;
+        double rotationAngle = 45;
+
+        string imgPath;
+
+        private int lastMouseX = -1;
+        private int lastMouseY = -1;
+        private bool isDragging = false;
+
+        public convert3D()
+        {
+            InitializeComponent();
+        }
+        private string openDriectory()
+        {
+            //파일오픈창 생성 및 설정
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "FileOpen";
+            ofd.FileName = "";
+            ofd.Filter = "JPEG File(*.jpg)|*.jpg|Bitmap File(*.bmp)|*.bmp|PNG File(*.png)|*.png";
+            
+            //Dialog Load
+            DialogResult dr = ofd.ShowDialog();
+
+            //DialogResult OK
+            if (dr == DialogResult.OK)
+            {
+                //File명.확장자를 Get
+                string fileName = ofd.SafeFileName;
+                //File경로와 File명을 Get
+                string fileFullName = ofd.FileName;
+
+                //label1에 파일명을 입력
+                label1.Text = "파일명 : " + fileName;
+                
+                //이미지 정보 (filename + Path)
+                return fileFullName;
+            }
+            //Dialog Result Cancel
+            else if (dr == DialogResult.Cancel)
+            {
+                return "";
+            }
+            return "";
+        }
+        public void Draw(WinChartViewer viewer)
+        {
+            Mat img = new Mat(imgPath);
+            Mat grayScale = new Mat(img.Rows, img.Cols, MatType.CV_8UC1);
+
+            grayScale = img.CvtColor(ColorConversionCodes.BGR2GRAY);
+            byte[,] data = new byte[grayScale.Rows, grayScale.Cols];
+
+            for (int i = 0; i < grayScale.Rows; i++)
+            {
+                for (int j = 0; j < grayScale.Cols; j++)
+                {
+                    data[i, j] = grayScale.At<Byte>(i, j);
+                }
+            }
+            double[] dataX = new double[grayScale.Rows * grayScale.Cols];
+            double[] dataY = new double[grayScale.Rows * grayScale.Cols];
+            double[] dataZ = new double[grayScale.Rows * grayScale.Cols]; //GrayValue
+
+            int SIZE = 0;
+            for (int x = 0; x < grayScale.Rows; x++)
+            {
+                for (int y = 0; y < grayScale.Cols; y++)
+                {
+                    dataX[SIZE] = x;
+                    dataY[SIZE] = y;
+                    dataZ[SIZE] = data[x, y];
+                    SIZE++;
+                }
+            }
+            
+            SurfaceChart c = new SurfaceChart(595, 400);
+            
+            c.setPlotRegion(240, 170, 250, 250, 180);
+            c.setData(dataX, dataY, dataZ);
+            c.setInterpolation(100, 100);
+            c.setViewAngle(elevationAngle, rotationAngle);
+            
+            c.setShadingMode(Chart.RectangularFrame);
+            c.setColorAxis(530, 270, Chart.Left, 200, Chart.Right);
+            c.setWallColor(0x000000);
+
+            c.setWallGrid(0xffffff, 0xffffff, 0xffffff, 0x888888, 0x888888, 0x888888);
+            c.setWallThickness(0, 0, 0);
+            c.setWallVisibility(true, false, false);
+
+            c.xAxis().setTitle("Y Axis", "Arial Bold", 15);
+            c.yAxis().setTitle("X Axis", "Arial Bold", 15);
+
+            c.xAxis().setLabelStyle("Arial", 10);
+            c.yAxis().setLabelStyle("Arial", 10);
+            c.zAxis().setLabelStyle("Arial", 10);
+            c.colorAxis().setLabelStyle("Arial", 10);
+
+            // Output the chart
+            viewer.Chart = c;
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            imgPath = openDriectory();
+            drawChart.updateViewPort(true, false);
+        }
+        private void drawChart_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (0 != (e.Button & MouseButtons.Left))
+            {
+                // Start Drag
+                isDragging = true;
+                lastMouseX = e.X;
+                lastMouseY = e.Y;
+                (sender as WinChartViewer).updateViewPort(true, false);
+            }
+        }
+        private void drawChart_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            { 
+                rotationAngle += (lastMouseX - e.X) * 90.0 / 360;
+                elevationAngle += (e.Y - lastMouseY) * 90.0 / 270;
+                lastMouseX = e.X;
+                lastMouseY = e.Y;
+                (sender as WinChartViewer).updateViewPort(true, false);
+            }
+        }
+        private void drawChart_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (0 != (e.Button & MouseButtons.Left))
+            {
+                // End Drag
+                isDragging = false;
+                (sender as WinChartViewer).updateViewPort(true, false);
+            }
+        }
+        private void drawChart_ViewPortChanged(object sender, ChartDirector.WinViewPortEventArgs e)
+        {
+            if (e.NeedUpdateChart)
+                Draw(drawChart);
+        }
+    }
+}
